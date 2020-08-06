dab022b83a1df62b228d0ce367e7762419fd2915
Alejandro Pedraza <alejandro@buoyant.io>
2020-08-06 17:19:04 -0500

    iptables rules were failing in CNI when using --use-wait-flag (#24)
    * iptables rules were failing in CNI when using --use-wait-flag
    
    Followup to #4
    
    `iptables-save` doesn't accept the `-w` argument, which is causing the
    script to error, when used in CNI.
    
    Also, properly escape nsenter args.
    
    Signed-off-by: Alejandro Pedraza <alejandro@buoyant.io>

--- iptables/iptables.go
+++ iptables/iptables.go
@@ -221,7 +221,7 @@ func makeMultiportDestinations(portsToIgnore []string) [][]string {
 }
 
 func executeCommand(firewallConfiguration FirewallConfiguration, cmd *exec.Cmd) error {
-	if firewallConfiguration.UseWaitFlag {
+	if strings.HasSuffix(cmd.Path, "iptables") && firewallConfiguration.UseWaitFlag {
 		log.Println("Setting UseWaitFlag: iptables will wait for xtables to become available")
 		cmd.Args = append(cmd.Args, "-w")
 	}
@@ -232,10 +232,11 @@ func executeCommand(firewallConfiguration FirewallConfiguration, cmd *exec.Cmd) 
 
 	// wrap up the cmd with nsenter if we were givin a netns
 	if len(firewallConfiguration.NetNs) > 0 {
-		cmd.Args = append([]string{
-			"nsenter",
-			fmt.Sprintf("--net=%s", firewallConfiguration.NetNs),
-		}, cmd.Args...)
+		nsenterArgs := []string{fmt.Sprintf("--net=%s", firewallConfiguration.NetNs)}
+		originalCmd := strings.Trim(fmt.Sprintf("%v", cmd.Args), "[]")
+		originalCmdAsArgs := strings.Split(originalCmd, " ")
+		finalArgs := append(nsenterArgs, originalCmdAsArgs...)
+		cmd = exec.Command("nsenter", finalArgs...)
 	}
 
 	log.Printf(":; %s\n", strings.Trim(fmt.Sprintf("%v", cmd.Args), "[]"))
