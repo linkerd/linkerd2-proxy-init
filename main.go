9ee9360a9ba9a1722224282a6c4c5e25cde83ab4
Eugene Formanenko <mo4islona@gmail.com>
2021-06-23 20:11:13 +0300

    Log to stdout instead of stderr (#34)
    Signed-off-by: Eugene Formanenko <mo4islona@gmail.com>

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/linkerd/linkerd2-proxy-init/cmd"
 )
 
 func main() {
+	log.SetOutput(os.Stdout)
+
 	if err := cmd.NewRootCmd().Execute(); err != nil {
 		os.Exit(1)
 	}
