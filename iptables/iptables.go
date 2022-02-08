d020fa55a2537f1fb2065825d6589ae3ba8c7a68
Matei David <matei@buoyant.io>
2022-02-08 10:27:35 +0000

    Update iptables/iptables.go
    Co-authored-by: Oliver Gould <ver@buoyant.io>

--- iptables/iptables.go
+++ iptables/iptables.go
@@ -84,7 +84,7 @@ func ConfigureFirewall(firewallConfiguration FirewallConfiguration) error {
 
 	for _, cmd := range commands {
 		if err := executeCommand(firewallConfiguration, cmd, nil); err != nil {
-			log.Error("aborting firewall configuration: %v", err)
+			log.Errorf("aborting firewall configuration: %v", err)
 			return err
 		}
 	}
