ab1ed291fd466bc2034ca375cb8ef6e7debec48a
Steve Jenson <stevej@buoyant.io>
2023-01-10 18:57:00 +0000

    cleanup unusued functions, run network-validator before test suite
    Signed-off-by: Steve Jenson <stevej@buoyant.io>

--- cni-plugin/integration/run.sh
+++ cni-plugin/integration/run.sh
@@ -24,9 +24,6 @@ function create_test_lab() {
 
 function cleanup() {
     echo '# Cleaning up...'
-    #k delete -f manifests/cni-plugin-lab.yaml
-    # TODO(stevej): how do we parameterize the linkerd-cni
-    # container image? we'd like to use releases as well as test
     k delete -f manifests/linkerd-cni.yaml
     k delete serviceaccount linkerd-cni
     k delete ns cni-plugin-test
@@ -34,39 +31,34 @@ function cleanup() {
 
 trap cleanup EXIT
 
-# Get the IP of a test pod.
-function kip() {
-    local name=$1
-    k wait pod "$name" --namespace=cni-plugin-test \
-        --for=condition=ready --timeout=1m \
-        >/dev/null
-
-    k get pod "$name" --namespace=cni-plugin-test \
-        --template='{{.status.podIP}}'
-}
-
 if k get ns/cni-plugin-test >/dev/null 2>&1 ; then
   echo 'ns/cni-plugin-test already exists' >&2
   exit 1
 fi
 
 create_test_lab
+
 # Wait for linkerd-cni daemonset to complete
 if ! k rollout status --timeout=30s daemonset/linkerd-cni -n linkerd-cni; then
   echo "!! linkerd-cni didn't rollout properly, check logs";
   exit $?
 fi
 
-#TODO(stevej): using the cni-plugin-lab as a manifest lets me exercise
-#the linkerd-network-validator but makes seeing the test output impossible.
-#
-#echo "# linkerd-cni is running, starting first cni-plugin test..."
-#k create -f manifests/cni-plugin-lab.yaml
-# Wait for cni-plugin-lab deployment to complete
-#if ! k rollout status --timeout=30s deployment/cni-plugin-tester-deployment -n cni-plugin-test; then
-#    echo "!! cni-plugin-tester-deployment failed, check logs"
-#    exit $?
-#fi
+# TODO(stevej): we don't want to rely on a linkerd build in this repo, we
+# can package network-validator separately.
+echo '# Run the network validator...'
+k run linkerd-proxy \
+    --attach \
+    -i \
+    --command \
+    --image="cr.l5d.io/linkerd/proxy:edge-22.12.1" \
+    --image-pull-policy=IfNotPresent \
+    --namespace=cni-plugin-test \
+    --restart=Never \
+    --rm \
+    -- \
+    /usr/lib/linkerd/linkerd2-network-validator --log-format plain --log-level debug --connect-addr 1.1.1.1:20001 --listen-addr 0.0.0.0:4140 --timeout 10s
+echo 'PASS: Network Validator'
 
 # This needs to use the name linkerd-proxy so that linkerd-cni will run.
 echo '# Running tester...'
@@ -76,6 +68,6 @@ k run linkerd-proxy \
         --image-pull-policy=Never \
         --namespace=cni-plugin-test \
         --restart=Never \
+        --rm \
         -- \
         go test -v ./cni-plugin/integration/... -integration-tests
-
