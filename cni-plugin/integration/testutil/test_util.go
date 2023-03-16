8e4ac2d441b5ef90ca2a5c8c0c991f0cc12df042
Steve Jenson <stevej@gmail.com>
2023-03-16 02:56:13 -0700

    Check that the linkerd config embedded in cni config is correct (#189)
    Added check that linkerd config embedded in cni config is correct
    
    
    Signed-off-by: Steve Jenson <stevej@buoyant.io>
    Co-authored-by: Steve Jenson <stevej@buoyant.io>
    Co-authored-by: Matei David <matei@buoyant.io>

--- cni-plugin/integration/testutil/test_util.go
+++ cni-plugin/integration/testutil/test_util.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+
+	"github.com/mitchellh/mapstructure"
 )
 
 // TestRunner is a helper struct used for cni-plugin integration test
@@ -37,6 +39,102 @@ func (r *TestRunner) walkConfDir() (map[string]struct{}, error) {
 	}
 
 	return fileNames, nil
+}
+
+// ProxyInit is the configuration for the proxy-init binary
+type ProxyInit struct {
+	IncomingProxyPort     int      `json:"incoming-proxy-port"`
+	OutgoingProxyPort     int      `json:"outgoing-proxy-port"`
+	ProxyUID              int      `json:"proxy-uid"`
+	PortsToRedirect       []int    `json:"ports-to-redirect"`
+	InboundPortsToIgnore  []string `json:"inbound-ports-to-ignore"`
+	OutboundPortsToIgnore []string `json:"outbound-ports-to-ignore"`
+	Simulate              bool     `json:"simulate"`
+	UseWaitFlag           bool     `json:"use-wait-flag"`
+}
+
+// LinkerdPlugin is what we use for CNI configuration in the plugins section
+type LinkerdPlugin struct {
+	Name       string         `json:"name"`
+	Type       string         `json:"type"`
+	LogLevel   string         `json:"log_level"`
+	Policy     map[string]any `json:"policy"`
+	Kubernetes map[string]any `json:"kubernetes"`
+	ProxyInit  ProxyInit      `json:"linkerd"`
+}
+
+// Checks that the embedded linkerd config is of the expected form
+// and contains the right values:
+//
+//	"linkerd": {
+//	   "incoming-proxy-port": 4143,
+//	   "outgoing-proxy-port": 4140,
+//	   "proxy-uid": 2102,
+//	   "ports-to-redirect": [],
+//	   "inbound-ports-to-ignore": ["4191","4190"],
+//	   "simulate": false,
+//	   "use-wait-flag": false
+//	 }
+func checkLinkerdCniConf(plugin map[string]any) error {
+	linkerdPlugin := &LinkerdPlugin{}
+	config := &mapstructure.DecoderConfig{
+		TagName:     "json",
+		Result:      linkerdPlugin,
+		ErrorUnused: true, // will fail if there are unparsed map elements
+	}
+
+	decoder, err := mapstructure.NewDecoder(config)
+	if err != nil {
+		return err
+	}
+
+	if err := decoder.Decode(plugin); err != nil {
+		return err
+	}
+
+	proxyInit := linkerdPlugin.ProxyInit
+	incomingProxyPort := proxyInit.IncomingProxyPort
+	if incomingProxyPort != 4143 {
+		return fmt.Errorf("incoming-proxy-port has wrong value, expected: %v, found: %v",
+			4143, incomingProxyPort)
+	}
+
+	outgoingProxyPort := proxyInit.OutgoingProxyPort
+	if outgoingProxyPort != 4140 {
+		return fmt.Errorf("outgoing-proxy-port has wrong value, expected: %v, found: %v",
+			4140, outgoingProxyPort)
+	}
+
+	proxyUID := proxyInit.ProxyUID
+	if proxyUID != 2102 {
+		return fmt.Errorf("proxy-uid has wrong value, expected: %v, found: %v", 2102, proxyUID)
+	}
+
+	simulate := proxyInit.Simulate
+	if simulate {
+		return fmt.Errorf("simulate has wrong value, expected: %v, found: %v", false, simulate)
+	}
+
+	useWaitFlag := proxyInit.UseWaitFlag
+	if useWaitFlag {
+		return fmt.Errorf("use-wait-flag has wrong value, expected: %v, found: %v",
+			false, useWaitFlag)
+	}
+
+	if len(proxyInit.PortsToRedirect) > 0 {
+		return fmt.Errorf("ports-to-redirect contains items and should not")
+	}
+
+	inboundPortsToIgnore := proxyInit.InboundPortsToIgnore
+
+	expectedInboundPortsToIgnore := [2]string{"4191", "4190"}
+	if inboundPortsToIgnore[0] != expectedInboundPortsToIgnore[0] ||
+		inboundPortsToIgnore[1] != expectedInboundPortsToIgnore[1] {
+		return fmt.Errorf("inbound-ports-to-ignore has wrong elements: found: %v, expected %v",
+			inboundPortsToIgnore, expectedInboundPortsToIgnore)
+	}
+
+	return nil
 }
 
 // CheckCNIPluginIsLast will, based on a configuration directory path, and a CNI
@@ -77,9 +175,14 @@ func (r *TestRunner) CheckCNIPluginIsLast() error {
 	}
 
 	plugins := conflist["plugins"].([]interface{})
-	lastPlugin := plugins[len(plugins)-1].(map[string]any)
-	if lastPlugin["name"] != "linkerd-cni" {
+	// check that the last plugin is linkerd
+	linkerdPlugin := plugins[len(plugins)-1].(map[string]any)
+	if linkerdPlugin["name"] != "linkerd-cni" {
 		return fmt.Errorf("linkerd-cni was not last in the plugins list")
+	}
+
+	if err = checkLinkerdCniConf(linkerdPlugin); err != nil {
+		return fmt.Errorf("Configuration contains bad values: %w", err)
 	}
 
 	return nil
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/containernetworking/cni v1.1.2
+	github.com/mitchellh/mapstructure v1.1.2
 	github.com/sirupsen/logrus v1.9.0
 	github.com/spf13/cobra v1.6.1
 	k8s.io/api v0.26.2
