22a22f5b3531212a8674dc351053701046a8d627
Alex Leong <alex@buoyant.io>
2026-05-05 16:55:12 +0000

    more waiting
    Signed-off-by: Alex Leong <alex@buoyant.io>

--- cni-repair-controller/integration/run.sh
+++ cni-repair-controller/integration/run.sh
@@ -59,7 +59,7 @@ linkerd check
 
 step 'Installing pause DaemonSet'
 kubectl apply -f pause-ds.yml
-kubectl wait --for=condition=ready --timeout=120s -l app=pause-app po
+kubectl wait --for=condition=ready --timeout=300s -l app=pause-app po
 
 step 'Adding a node'
 cluster=$(just-k3d --evaluate K3D_CLUSTER_NAME)
@@ -68,11 +68,13 @@ k3d node create node2 --cluster "$cluster"
 k3d image import --cluster "$cluster" "$image"
 
 step 'Checking new DS replica fails with code 95'
-sleep 10
-kubectl wait \
+until kubectl wait \
   --for=jsonpath='{.status.initContainerStatuses[0].lastState.terminated.exitCode}'=95 \
   --field-selector=spec.nodeName=k3d-node2-0 \
-  pod
+  pod; do
+  echo "Waiting for new DS replica to fail with code 95..."
+  sleep 5
+done
 
 step 'Checking new DS replica gets replaced'
 for _ in {1..5}; do
