96536c6ee81a806f96aa0b003783763951fc8470
Raymond Kroeker <raymond@buoyant.io>
2026-06-16 16:12:12 -0700

    feat(cni-installer): make log-level a flag

--- cni-install/main.go
+++ cni-install/main.go
@@ -4,6 +4,8 @@ package main
 import (
 	"context"
 	"errors"
+	"flag"
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -12,7 +14,21 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+const (
+	// defaultLevel is used if the provided level cannot be parsed.
+	defaultLevel = logrus.DebugLevel
+)
+
+// flags provided by parsing command line args.
+var flags struct {
+	// logLevel override
+	logLevel string
+}
+
 func main() {
+	flag.StringVar(&flags.logLevel, "log-level", defaultLevel.String(),
+		fmt.Sprintf("installer log level: %q", logrus.AllLevels))
+	flag.Parse()
 	ctx, cancel := context.WithCancel(context.Background())
 
 	ch := make(chan os.Signal, 1)
@@ -22,7 +38,13 @@ func main() {
 	}()
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
 
-	logrus.SetLevel(logrus.DebugLevel)
+	level, err := logrus.ParseLevel(flags.logLevel)
+	if err != nil {
+		_, _ = fmt.Fprintf(flag.CommandLine.Output(),
+			"Cannot parse log-level '%s'\n", flags.logLevel)
+		os.Exit(1)
+	}
+	logrus.SetLevel(level)
 	logrus.Info("running installer")
 	installer := cni.NewInstaller()
 	defer func() {
@@ -30,7 +52,7 @@ func main() {
 			logrus.WithError(err).Fatal("cannot uninstall cni")
 		}
 	}()
-	err := installer.Run(ctx)
+	err = installer.Run(ctx)
 	if err != nil && !errors.Is(err, context.Canceled) {
 		logrus.WithError(err).Fatal("cannot run cni install")
 	}
